0888f484126d340f35bd447b130ca44010153780
Kostadin Sajic <kokiftw93@gmail.com>
2023-01-04 12:28:20 +0100

    initial commit

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+      // specify options here
+    });
+  });
